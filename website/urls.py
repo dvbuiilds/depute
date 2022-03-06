c74ec8c3313f2041915da58888b366f5b677b94d
Sincere-Dhairya <varshneydhairya4@gmail.com>
2022-03-06 22:21:55 +0530

    06mar22

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = "index"),
+    path('about-us', views.about, name = "about-us"),
+    path('outsource-to-us', views.outsource, name="outsource-to-us"),
+    path('hire-with-us', views.hiring, name="hire-with-us"),
 ]
--- website/views.py
+++ website/views.py
@@ -4,4 +4,13 @@ from django.http import HttpResponse, HttpResponseServerError
 
 def index(request):
     return render(request, 'index.html')
+
+def outsource(request):
+    return render(request, 'outsource.html')
+
+def hiring(request):
+    return render(request, 'hire.html')
+
+def about(request):
+    return render(request, 'about.html')
 # Create your views here.
