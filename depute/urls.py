405a140cf1d91f422e57e04c8d10e82cf10f6267
Sincere-Dhairya <varshneydhairya4@gmail.com>
2023-07-01 01:06:56 +0530

    responsive ness

--- depute/urls.py
+++ depute/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
 urlpatterns = [
@@ -23,6 +24,8 @@ urlpatterns = [
     path('', include('recruiter.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
 # first argument in the path() function is route, second is view
 
 # route is the string that the django starts matching in the following urlpatterns
